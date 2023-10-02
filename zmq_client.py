01f6b6957cf21dd9033c14a9fca00fa4e0c748fd
Adam Katav <adamkatav@gmail.com>
2023-10-02 21:27:38 +0300

    shit

--- zmq_client.py
+++ zmq_client.py
@@ -3,8 +3,8 @@ import json
 from typing import NamedTuple
 context = zmq.Context()
 socket = context.socket(zmq.SUB)
-socket.setsockopt(zmq.SUBSCRIBE, b'')
-socket.connect("tcp://192.168.0.208:5050")
+socket.setsockopt(zmq.SUBSCRIBE, b'acc')
+socket.connect("tcp://192.168.85.128:5050")
 
 class Sample(NamedTuple):
     x: float
@@ -16,11 +16,10 @@ class Sample(NamedTuple):
     timestamp: float
 
 
-
-wanted_keys = ['bvp', 'ibi', 'x', 'y', 'z', 'tag', 'timestamp']
 while True:
     #  Wait for next request from client
-    msg = json.loads(socket.recv())
-    msg = {k:(-1 if msg[k]=='' else float(msg[k])) for k in wanted_keys}
-    sample = Sample(**msg)
-    print(f'Received {sample=}')
+    # msg = json.loads(socket.recv())
+    # sample = Sample(**msg)
+    # print(f'Received {sample=}')
+    msg = str(socket.recv()).split(' ')[1]
+    print(f'Received {msg=}')
